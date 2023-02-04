c58f68897a4f5657518b6ff3b1feea7d37335543
Dmitriy Akulov <dakulovgr@gmail.com>
2023-02-04 16:39:20 +0100

    fix: setup-node@v3

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
 	branches: [ 'master' ],
-	repositoryUrl: 'https://github.com/jsdelivr/globalping-probe.git',
+	repositoryUrl: 'https://github.com/zarianec/globalping-probe.git',
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
